cd2d2383672610ebf088f3ff7f65eb6ed1c54330
kai.1001 <kai.1001@kakaocorp.com>
2018-06-20 19:40:47 +0900

    setup router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './containers/App';
+import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
