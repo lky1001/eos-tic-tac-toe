3254e1d5e46624f917235814c98d00773479167c
kai.1001 <kai.1001@kakaocorp.com>
2018-06-20 19:50:15 +0900

    Setting routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from '../router/Routes';
 import logo from '../static/logo.svg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <div style={{marginTop: '4rem'}}>
+            <Routes />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
