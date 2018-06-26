ae9b4027a00ff4e4df05cea98cab139fd3988940
kai.1001 <kai.1001@kakaocorp.com>
2018-06-26 18:14:46 +0900

    With scatter

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,17 +5,15 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import EOS from 'eosjs';
+import * as Eos from 'eosjs';
 
 // eos contract account
 const CONTRACT_NAME = "tic.tac.toe";
-// eos contract execute account, pair with keyProvider
-const CONTRACT_SENDER = "tic.tac.toe";
 
-const EOS_CONFIG = {
-    // private key
-    keyProvider: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',
-    httpEndpoint: 'http://127.0.0.1:8888'
+const requiredFields = {
+    accounts:[
+        {blockchain:'eos', host:'127.0.0.1', port:8888}
+    ]
 };
 
 const BOARD_SIZE = 9;
@@ -26,6 +24,7 @@ class Game extends Component {
         super(props);
 
         this.state = {
+            isLogin: false,
             isPlay: false,
             challenger: 'inita',
             host: 'initb',
@@ -35,7 +34,38 @@ class Game extends Component {
             isLoading: false
         };
 
-        this.eos = EOS(EOS_CONFIG);
+        //this.eos = EOS(EOS_CONFIG);
+
+        let scatterNetwork = {
+            protocol:'http',
+            blockchain: 'eos',
+            host: '127.0.0.1',
+            port: 8888,
+            chainId: "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f"
+        };
+
+        let config = {
+            broadcast: true,
+            sign: true,
+            chainId: "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f"
+        };
+
+        document.addEventListener('scatterLoaded', scatterExtension => {
+            this.scatter = window.scatter;            
+            //window.scatter = null;
+
+            this.eos = this.scatter.eos(scatterNetwork, Eos, config);
+
+            if (!this.scatter.identity) {
+                this.setState({
+                    isLogin: false
+                });
+            } else {
+                this.setState({
+                    isLogin: true
+                });
+            }
+        });
     }
 
     handleChange = name => event => {
@@ -56,7 +86,7 @@ class Game extends Component {
                 let host = this.state.host;
                 let challenger = this.state.challenger;
 
-                this.eos.contract(CONTRACT_NAME).then((contract) => {
+                this.eos.contract(CONTRACT_NAME, requiredFields).then((contract) => {
                     contract.create(
                         challenger,
                         host,
@@ -207,6 +237,18 @@ class Game extends Component {
         // todo
     };
 
+    getScatterId = () => {
+        this.scatter.getIdentity().then(id => {
+            if(!id) return false;
+            this.scatter.useIdentity(id.hash);
+            console.log('Possible identity', id);
+            this.setState({
+                isLogin: true,
+                identity: id
+            });
+        });
+    };
+
     render() {
         return(
             <div>
@@ -224,15 +266,21 @@ class Game extends Component {
                             <Board board={this.state.board}
                                 handleClick={this.move} />
                         </div>
-                        <Button name="start" onClick={this.restartGame} variant="contained" color="primary">Restart Game</Button> <br /><br />
+                        <Button name="restart" onClick={this.restartGame} variant="contained" color="primary">Restart Game</Button> <br /><br />
                         {
                             this.state.turn === this.state.host &&
-                            <Button name="start" onClick={this.closeGame} variant="contained" color="primary">Close Game</Button>
+                            <Button name="close" onClick={this.closeGame} variant="contained" color="primary">Close Game</Button>
                         }
                     </div>
                 }
                 {
-                    !this.state.isPlay && 
+                    !this.state.isLogin &&
+                    <div>
+                        <Button name="login" onClick={this.getScatterId} variant="contained" color="primary">Login</Button>
+                    </div>
+                }
+                {
+                    this.state.isLogin && !this.state.isPlay && 
                     <div>
                         <form className="player-info-container" noValidate autoComplete="off">
                             <div className="field-margin">
@@ -248,7 +296,7 @@ class Game extends Component {
                                             placeholder="host account"
                                             className="player-field"
                                             onChange={this.handleChange('host')}
-                                            value="initb"
+                                            value={this.state.host}
                                             margin="normal" />
                                     </Grid>
                                 </Grid>
@@ -264,7 +312,7 @@ class Game extends Component {
                                             placeholder="challenger account"
                                             className="player-field"
                                             onChange={this.handleChange('challenger')}
-                                            value="inita"
+                                            value={this.state.challenger}
                                             margin="normal" />
                                     </Grid>
                                 </Grid>
